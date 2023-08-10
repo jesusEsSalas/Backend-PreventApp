8435e6d5af4c159d29ccdee09607bb3cdd4e1982
Jesus Salas <70954436+jesusEsSalas@users.noreply.github.com>
2023-08-10 13:06:46 -0600

    Requirements

---
+++ dummy.py
@@ -0,0 +1,14 @@
+static_problems = [
+    {'history': 'Collapsed traffic from Zapopan Centro Cemetery to Periférico at Plaza Belenes, take precautions when driving through the area ???????? #ReporteZMG https://t.co/PsC5eocpro',
+     'topics': ['traffic accident']},
+    {'history': 'Fire at the height of Preparatory School No. 20 UdeG, on Paso del Bosque Avenue, in the El Fortín neighborhood.We ask @UMPCyBZ to attend to the report as soon as possible. #ReporteZMG',
+     'topics': ['fire']},
+    {'history': 'Traffic light not working at the intersection of Cristobal Colon Avenue and Paseo de los Balcones Street, in the Cerro del Tesoro neighborhood in Tlaquepaque 🚥 We ask @AgenciaAMIM and @JaliscoVial to attend to the report. #ReporteZMG https://t.co/EKTvFvo5Gt',
+     'topics': ['traffic light']},
+    {'history': 'Heavy collision between public transportation unit and private vehicle on Calderón de la Barca Street and José María Morelos Avenue #ReporteZMG https://t.co/QIDC6aPMFw',
+     'topics': ['traffic accident']},
+    {'history': 'A vehicle caught fire on Mariano Otero Avenue, near Expo Guadalajara, in the direction of López Mateos Avenue. The incident has been controlled, however, the roadway has a lot of traffic in both directions.',
+     'topics': ['fire']},
+    {'history': 'Traffic light obstructed by tree leaves at the intersection of Franciscode Ayza and Felipe Ángeles streets.',
+     'topics': ['traffic light']}
+]
