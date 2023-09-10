269d9dc22d38d8ad08e42c3214bec9a5b619f810
Jesus Salas <70954436+jesusEsSalas@users.noreply.github.com>
2023-09-10 08:46:28 -0600

    Inster the data into db

--- api.py
+++ api.py
@@ -3,14 +3,24 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 import re
+import pyodbc
+import datetime
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from googletrans import Translator
+from dummy import accidents
 
 app = Flask(__name__)
 binary_model = tf.keras.models.load_model('training/BERT_Model.h5', custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
 multiclass_model = tf.keras.models.load_model('training/BERT_Model_MultiClass.h5', custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
+
+cnxn_str = ("Driver={ODBC Driver 18 for SQL Server};"
+            "Server=p1wg10891.dc01.its.hpecorp.net,1121;"
+            "Database=AMS_AMR;"
+            "TrustServerCertificate=yes;"
+            "UID=dccadmin;"
+            "PWD=AM$GDLMay;")
 
 CORS(app)
 
@@ -49,30 +59,53 @@ def clean_text(text):
 
 @app.route("/predictedTopics", methods=["GET"])
 def getPredictedTopics():
-    #json = request.get_json()
+    json = request.get_json()
     #data = json['text']
-    data = ""
-    data = clean_text(data)
-    text = translate_text(data)
-    
-    predictions_list = [f'{text}']
-    predictions = binary_model.predict(predictions_list)
-    predictions = np.round(predictions.T[0])
-    accident = ''
-    if predictions[0] == 0.:
-        is_accident = False
-        return jsonify({'text': text, 'predictions': is_accident})
-    else:
-        is_accident = True
-        accident_predictions = predict_class(predictions_list)
-        if accident_predictions[0] == 0:
-            accident = 'traffic accident'
-        elif accident_predictions[0] == 1:
-            accident = 'fire'
-        elif accident_predictions[0] == 2:
-            accident = 'traffic light'
+    data = accidents
+    predictions_list = []
+    date = ''
+    for text in data:
+        description = text["descripción"]
+        description = clean_text(description)
+        description = [translate_text(description)]
+        date = datetime.datetime.strptime(text["fecha"], '%d/%m/%Y %I:%M %p')
+        predictions = binary_model.predict(description)
+        predictions = np.round(predictions.T[0])
         
-        return jsonify({'text': text, 'predictions': is_accident, 'type': accident})
+        if predictions[0] == 0.:
+            continue
+        else:
+            accident_predictions = predict_class(description)
+            if accident_predictions[0] == 0:
+                accident = 1
+            elif accident_predictions[0] == 1:
+                accident = 2
+            elif accident_predictions[0] == 2:
+                accident = 3
+            
+            prediction = {
+                "text": text["descripción"],
+                "predictions": accident,
+                "date": date,
+                "state": 0
+            }
+            predictions_list.append(prediction)
+    
+    each_prediction = []
+    for item in predictions_list:
+        predictions_tuple = (item["text"], item["predictions"], item["date"], item["state"])
+        each_prediction.append(predictions_tuple)
+    try:
+        cnxn = pyodbc.connect(cnxn_str)
+        cursor = cnxn.cursor()
+        query = f"INSERT INTO [dbo].[zzDB] ([Descripcion], [CtegoriaId], [Fecha], [Estado]) VALUES (?, ?, ?, ?)"
+        cursor.executemany(query, each_prediction)
+        cnxn.commit()
+        cnxn.close()
+    except:
+        print("No hay publicaciones de precances viales")
+    return predictions_list
+    
 
 
 if __name__ == "__main__":
